a49b9e7a2443ca4205c9ccea1c4d7bdeaf0c77ef
Javi Armendáriz <katanagari@gmail.com>
2019-10-06 21:48:50 +0200

    Added support to bidimensional boards
    Unidimensional boards are not used apart from the tests, so I made
    no effort to adapt the game engine

--- pacman/game.py
+++ pacman/game.py
@@ -4,37 +4,61 @@ from pacman.pacman import Direction
 # Game tick function. Receives current game state
 # and should return the new state
 def tick(current_state, direction):
-    new_state = list(current_state)
-    pacman_index = __find_pacman_position(new_state)
+    new_state = __copy_state(current_state)
+    pacman_x, pacman_y = __find_pacman_position(new_state)
 
     if direction is not None:
-        new_state[pacman_index] = ' '
+        new_state[pacman_x][pacman_y] = ' '
 
-    (pacman_new_index, pacman) = __calculate_pacman_new_position(new_state, pacman_index, direction)
-    new_state[pacman_new_index] = pacman
+    (pacman_new_x, pacman_new_y, pacman) = __calculate_pacman_new_position(new_state, (pacman_x, pacman_y), direction)
+    new_state[pacman_new_x][pacman_new_y] = pacman
 
-    return ''.join(new_state)
+    return __convert_to_output_state(new_state)
+
+
+def __copy_state(current_state):
+    new_state = []
+    for row in current_state:
+        new_state.append(list(row))
+
+    return new_state
+
+
+def __convert_to_output_state(state):
+    output_state = []
+    for row in state:
+        output_state.append(''.join(row))
+
+    return tuple(output_state)
 
 
 def __find_pacman_position(state):
-    for i, obj in enumerate(state):
-        if obj in ['v', '<', '^', '>']:
-            return i
+    for i, row in enumerate(state):
+        for j, item in enumerate(row):
+            if item in ['v', '<', '^', '>']:
+                return i, j
 
 
 def __calculate_pacman_new_position(state, pacman_position, direction):
-    new_position = pacman_position
-    new_pacman = state[pacman_position]
-    board_width = len(state)
+    new_position = list(pacman_position)
+    new_pacman = state[pacman_position[0]][pacman_position[1]]
+    board_width = len(state[0])
+    board_height = len(state)
 
     if direction == Direction.RIGHT:
-        new_position = (pacman_position + 1) % board_width
+        new_position[1] = (pacman_position[1] + 1) % board_width
         new_pacman = '<'
     elif direction == Direction.LEFT:
-        new_position = (pacman_position - 1) % board_width
+        new_position[1] = (pacman_position[1] - 1) % board_width
         new_pacman = '>'
+    elif direction == Direction.UP:
+        new_position[0] = (pacman_position[0] - 1) % board_height
+        new_pacman = 'v'
+    elif direction == Direction.DOWN:
+        new_position[0] = (pacman_position[0] + 1) % board_height
+        new_pacman = '^'
 
-    if state[new_position] == '#':
+    if state[new_position[0]][new_position[1]] == '#':
         new_position = pacman_position
 
-    return new_position, new_pacman
+    return new_position[0], new_position[1], new_pacman
--- tests/test_game.py
+++ tests/test_game.py
@@ -3,39 +3,67 @@ from pacman.pacman import Direction
 import pacman.game as game
 
 
-# TODO It would be better if we create an abstraction of the board?
+# TODO It would be better if I create an abstraction of the board?
 # TODO even though I have to rewrite all tests :-(
 class TestGame(unittest.TestCase):
     def test_pacman_dont_move_at_beginning(self):
-        board = '.v.'
+        board = ('.v.',
+                 '...')
         new_board = game.tick(board, None)
         self.assertEqual(board, new_board)
 
     def test_pacman_moves_right_when_input_given(self):
-        board = '.v.'
-        expected = '. <'
+        board = ('.v.', '...')
+        expected = ('. <', '...')
         new_board = game.tick(board, Direction.RIGHT)
         self.assertEqual(expected, new_board)
 
     def test_pacman_moves_left_when_input_given(self):
-        board = '.v.'
-        expected = '> .'
+        board = ('.v.', '...')
+        expected = ('> .', '...')
         new_board = game.tick(board, Direction.LEFT)
         self.assertEqual(expected, new_board)
 
     def test_pacman_passes_through_empty_borders(self):
-        board = '.v.'
-        expected = '  >'
+        board = ('.v.', '...')
+        expected = ('  >', '...')
         step1 = game.tick(board, Direction.LEFT)
         new_board = game.tick(step1, Direction.LEFT)
 
         self.assertEqual(expected, new_board)
 
     def test_pacman_stops_at_wall(self):
-        board = '#..v..#'
-        expected = '#..  <#'
+        board = ('#..v..#', '#.....#')
+        expected = ('#..  <#', '#.....#')
         step1 = game.tick(board, Direction.RIGHT)
         step2 = game.tick(step1, Direction.RIGHT)
         new_board = game.tick(step2, Direction.RIGHT)
 
+        self.assertEqual(expected, new_board)
+
+    def test_pacman_can_move_up(self):
+        board = ('#.....#',
+                 '#..v..#')
+        expected = ('#..v..#',
+                    '#.. ..#')
+        new_board = game.tick(board, Direction.UP)
+        self.assertEqual(expected, new_board)
+
+    def test_pacman_can_wrap_vertically(self):
+        board = ('#.....#',
+                 '#..v..#')
+        expected = ('#.. ..#',
+                    '#..v..#')
+
+        step1 = game.tick(board, Direction.UP)
+        new_board = game.tick(step1, Direction.UP)
+        self.assertEqual(expected, new_board)
+
+    def test_pacman_cannot_pass_through_a_wall(self):
+        board = ('#..#..#',
+                 '#..v..#')
+        expected = ('#..#..#',
+                    '#..^..#')
+
+        new_board = game.tick(board, Direction.DOWN)
         self.assertEqual(expected, new_board)
